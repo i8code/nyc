05ca7b1df8d8d662faf0aa2ae5ebd168ad726aa5
i8code <rsteinbergis@gmail.com>
2013-11-16 20:38:39 -0500

    Initial commit;

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nyc::Application.config.secret_key_base = 'bff1e5b20d67c1da4c7dfa2692835554a399cd4f91c7dcac21d0273d586304d31a25ce130a10ab8ceba0aa113610b3f789c6196116117c0bcddb06a5a20b39cb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Nyc::Application.config.session_store :cookie_store, key: '_nyc_session'
